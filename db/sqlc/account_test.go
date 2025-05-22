5d4309a7020d808132272124391b99f82b1d7377
gumeeee <gaxco123@gmail.com>
2025-05-22 20:30:46 -0300

    feat: adding Account unit tests, util random datas for tests and implementing Makefile

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,16 +2,19 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
+	"github.com/gumeeee/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	params := CreateAccountParams{
-		Owner:    "john doe",
-		Balance:  100,
-		Currency: "BRL",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), params)
@@ -25,4 +28,72 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	accountSaved := createRandomAccount(t)
+	accountSearched, err := testQueries.GetAccount(context.Background(), accountSaved.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, accountSearched)
+
+	require.Equal(t, accountSaved.ID, accountSearched.ID)
+	require.Equal(t, accountSaved.Owner, accountSearched.Owner)
+	require.Equal(t, accountSaved.Balance, accountSearched.Balance)
+	require.Equal(t, accountSaved.Currency, accountSearched.Currency)
+	require.WithinDuration(t, accountSaved.CreatedAt, accountSearched.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	accountSaved := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      accountSaved.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	accountUpdated, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, accountUpdated)
+
+	require.Equal(t, accountSaved.ID, accountUpdated.ID)
+	require.Equal(t, accountSaved.Owner, accountUpdated.Owner)
+	require.Equal(t, arg.Balance, accountUpdated.Balance)
+	require.Equal(t, accountSaved.Currency, accountUpdated.Currency)
+	require.WithinDuration(t, accountSaved.CreatedAt, accountUpdated.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	accountSaved := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), accountSaved.ID)
+	require.NoError(t, err)
+
+	accountDeleted, err := testQueries.GetAccount(context.Background(), accountSaved.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, accountDeleted)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
--- util/random.go
+++ util/random.go
@@ -3,14 +3,9 @@ package util
 import (
 	"math/rand"
 	"strings"
-	"time"
 )
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
 
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
@@ -20,7 +15,7 @@ func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
-	for i := 0; 1 < n; i++ {
+	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
