aa82c59b9c51d725c93ea9f725811c00f2016870
gumeeee <gaxco123@gmail.com>
2025-06-30 22:07:43 -0300

    chore: add environment configuration and configuration variable loading support

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/gumeeee/simplebank/api"
 	db "github.com/gumeeee/simplebank/db/sqlc"
+	"github.com/gumeeee/simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	connection, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	connection, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(connection)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
