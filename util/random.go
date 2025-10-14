d7eba309a57b9d9757a0add619c8d5c6961e32a9
gumeeee <gaxco123@gmail.com>
2025-10-14 00:25:22 -0300

    feat: implement account transfer functionality and currency validation

--- util/random.go
+++ util/random.go
@@ -32,7 +32,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"BRL", "USD", "EUR"}
+	currencies := []string{EUR, USD, BRL}
 	n := len(currencies)
 
 	return currencies[rand.Intn(n)]
